7e9da959229b201dcaf0388619fb1996bd406f38
Senthil Ramakrishnan <Senthil.Ramakrishnan@arm.com>
2019-10-12 12:35:29 -0500

    Added maxkcoloring test

--- src/opt/test/MaxKColoringTest.java
+++ src/opt/test/MaxKColoringTest.java
@@ -31,6 +31,10 @@ import opt.prob.GenericProbabilisticOptimizationProblem;
 import opt.prob.MIMIC;
 import opt.prob.ProbabilisticOptimizationProblem;
 import shared.FixedIterationTrainer;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * 
@@ -70,47 +74,132 @@ public class MaxKColoringTest {
         CrossoverFunction cf = new SingleCrossOver();
         HillClimbingProblem hcp = new GenericHillClimbingProblem(ef, odd, nf);
         GeneticAlgorithmProblem gap = new GenericGeneticAlgorithmProblem(ef, odd, mf, cf);
-        
         Distribution df = new DiscreteDependencyTree(.1); 
         ProbabilisticOptimizationProblem pop = new GenericProbabilisticOptimizationProblem(ef, odd, df);
+        String csvTime = "";
+        String csvFitness = "";
+        long starttime, sumOpt, sumTime;
+        int loopCount = 1, i=0, j=0;
+        int[] iterations = {50, 100, 500, 1000, 2000, 5000, 10000, 20000};
+        csvTime += "AlgoTime";
+        csvFitness += "AlgoOptimal";
+        for(i=0; i<iterations.length;i++) {
+            csvTime += ("," + Integer.toString(iterations[i]));
+            csvFitness += ("," + Integer.toString(iterations[i]));  
+        }
+        csvFitness += "\n";
+        csvTime += "\n";
+
+        //////////////////////////////////////////////////////////////////////
+        csvTime += "RHC";
+        csvFitness += "RHC";
+        for(i=0; i<iterations.length;i++) {
+            sumOpt = 0;
+            sumTime = 0;
+            for(j=0; j<loopCount; j++) {
+                starttime = System.currentTimeMillis();
+                RandomizedHillClimbing rhc = new RandomizedHillClimbing(hcp);      
+                FixedIterationTrainer fit = new FixedIterationTrainer(rhc, iterations[i]);
+                fit.train();
+                sumOpt += ef.value(rhc.getOptimal());
+                sumTime += System.currentTimeMillis() - starttime;
+            }
+            System.out.println("RHC Optimal : " + sumOpt / loopCount);
+            System.out.println("Time : " + sumTime / loopCount);
+            csvFitness += ("," + ef.foundMax());
+            csvTime += ("," + (sumTime / loopCount)); 
+        }
+        csvFitness += "\n";
+        csvTime += "\n";
+        ////////////////////////////////////////////////////////////////////////
+
+        csvTime += "SA";
+        csvFitness += "SA";
+        for(i=0; i<iterations.length;i++) {
+            sumOpt = 0;
+            sumTime = 0;
+            for(j=0; j<loopCount; j++) {
+                starttime = System.currentTimeMillis();
+                SimulatedAnnealing sa = new SimulatedAnnealing(1E12, .1, hcp);
+                FixedIterationTrainer fit = new FixedIterationTrainer(sa, iterations[i]);
+                fit.train();
+                sumOpt += ef.value(sa.getOptimal());
+                sumTime += System.currentTimeMillis() - starttime;
+            }
+            System.out.println("SA Optimal : " + sumOpt / loopCount);
+            System.out.println("Time : " + sumTime / loopCount);
+            csvFitness += ("," + ef.foundMax());
+            csvTime += ("," + (sumTime / loopCount)); 
+        }
+        csvFitness += "\n";
+        csvTime += "\n";
+        ////////////////////////////////////////////////////////////////////////
+
+        csvTime += "GA";
+        csvFitness += "GA";
+        for(i=0; i<iterations.length;i++) {
+            sumOpt = 0;
+            sumTime = 0;
+            for(j=0; j<loopCount; j++) {
+                starttime = System.currentTimeMillis();
+                StandardGeneticAlgorithm ga = new StandardGeneticAlgorithm(200, 10, 60, gap);      
+                FixedIterationTrainer fit = new FixedIterationTrainer(ga, iterations[i]);
+                fit.train();
+                sumOpt += ef.value(ga.getOptimal());
+                sumTime += System.currentTimeMillis() - starttime;
+            }
+            System.out.println("GA Optimal : " + sumOpt / loopCount);
+            System.out.println("Time : " + sumTime / loopCount);
+            csvFitness += ("," + ef.foundMax());
+            csvTime += ("," + (sumTime / loopCount)); 
+        }
+        csvFitness += "\n";
+        csvTime += "\n";
+        /////////////////////////////////////////////////////////////////////////
+
+        csvTime += "MIMIC";
+        csvFitness += "MIMIC";
+        for(i=0; i<iterations.length;i++) {
+            sumOpt = 0;
+            sumTime = 0;
+            for(j=0; j<loopCount; j++) {
+                starttime = System.currentTimeMillis();
+                MIMIC mimic = new MIMIC(200, 100, pop);      
+                FixedIterationTrainer fit = new FixedIterationTrainer(mimic, iterations[i]);
+                fit.train();
+                sumOpt += ef.value(mimic.getOptimal());
+                sumTime += System.currentTimeMillis() - starttime;
+            }
+            System.out.println("MIMIC Optimal : " + sumOpt / loopCount);
+            System.out.println("Time : " + sumTime / loopCount);
+            csvFitness += ("," + ef.foundMax());
+            csvTime += ("," + (sumTime / loopCount)); 
+        }
+        csvFitness += "\n";
+        csvTime += "\n";
+        ///////////////////////////////////////////////////////////////////////////
         
-        long starttime = System.currentTimeMillis();
-        RandomizedHillClimbing rhc = new RandomizedHillClimbing(hcp);      
-        FixedIterationTrainer fit = new FixedIterationTrainer(rhc, 20000);
-        fit.train();
-        System.out.println("RHC: " + ef.value(rhc.getOptimal()));
-        System.out.println(ef.foundConflict());
-        System.out.println("Time : "+ (System.currentTimeMillis() - starttime));
+        System.out.println(csvFitness);
+        System.out.println(csvTime);
+
+        try {
+            File file = new File("maxkcolor_opt.csv");
+            file.createNewFile();
+            BufferedWriter csvFitnessFile = new BufferedWriter(new FileWriter(file));
+            csvFitnessFile.write(csvFitness);
+            csvFitnessFile.close();
+        } catch (IOException ioe) {
+	        ioe.printStackTrace();
+	    }
         
-        System.out.println("============================");
-        
-        starttime = System.currentTimeMillis();
-        SimulatedAnnealing sa = new SimulatedAnnealing(1E12, .1, hcp);
-        fit = new FixedIterationTrainer(sa, 20000);
-        fit.train();
-        System.out.println("SA: " + ef.value(sa.getOptimal()));
-        System.out.println(ef.foundConflict());
-        System.out.println("Time : "+ (System.currentTimeMillis() - starttime));
-        
-        System.out.println("============================");
-        
-        starttime = System.currentTimeMillis();
-        StandardGeneticAlgorithm ga = new StandardGeneticAlgorithm(200, 10, 60, gap);
-        fit = new FixedIterationTrainer(ga, 50);
-        fit.train();
-        System.out.println("GA: " + ef.value(ga.getOptimal()));
-        System.out.println(ef.foundConflict());
-        System.out.println("Time : "+ (System.currentTimeMillis() - starttime));
-        
-        System.out.println("============================");
-        
-        starttime = System.currentTimeMillis();
-        MIMIC mimic = new MIMIC(200, 100, pop);
-        fit = new FixedIterationTrainer(mimic, 5);
-        fit.train();
-        System.out.println("MIMIC: " + ef.value(mimic.getOptimal()));  
-        System.out.println(ef.foundConflict());
-        System.out.println("Time : "+ (System.currentTimeMillis() - starttime));
-        
+        try {
+            File file = new File("maxkcolor_time.csv");
+            file.createNewFile();
+            BufferedWriter csvTimeFile = new BufferedWriter(new FileWriter(file));
+            csvTimeFile.write(csvTime);
+            csvTimeFile.close();
+        } catch (IOException ioe) {
+	        ioe.printStackTrace();
+	    }
     }
 }
